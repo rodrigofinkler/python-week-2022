a658944c527150a69d8df5026de265622fd3d219
Rodrigo Finkler <finklerrg@gmail.com>
2025-11-16 22:48:19 -0300

    Initial modeling of the beer class

---
+++ beerlog/models.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field 
+from pydantic import validator
+from statistics import mean
+from datetime import datetime
+class Beer(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+    rating: int = 0
+    date: datetime = Field(default_factory=datetime.now)
+
+    @validator("flavor", "image", "cost")
+    def validate_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise RuntimeError(f"{field.name} must be between 1 and 10")
+        return v
+    
+    @validator("rating", always=True)
+    def calculate_rating(cls, v, values):
+        rating = mean([values["flavor"], values["image"], values["cost"]])
+        return int(rating)
+
+brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=8)
