06d261af8b1d90e224bf6254848472ae76b5aa88
Rodrigo Finkler <finklerrg@gmail.com>
2025-11-18 23:05:47 -0300

    Implement core functionality to add beer via CLI

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,23 @@
-from .config import settings
+import typer
+from beerlog.core import add_beer_to_database
 
+main = typer.Typer(help="Beer Management System")
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...)
+):
+    """Adds a new beer to database"""
+    if (add_beer_to_database(name, style, flavor, image, cost)):
+        print("🍺 beer added to database")
+    else:
+        print("🚫 failed to add beer")
+
+@main.command("list")
+def list_beers(style: str):
+    """Lists beers in database"""
+    print(style)
