d2087bc1f8eb3ece00157a68622256b462787c9a
Rodrigo Finkler <finklerrg@gmail.com>
2025-11-20 09:54:09 -0300

    beerlog list command now renders a formatted table

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,8 +1,12 @@
 import typer
 from typing import Optional
 from beerlog.core import add_beer_to_database, get_beers_from_database
+from rich.table import Table
+from rich.console import Console
 
 main = typer.Typer(help="Beer Management System")
+
+console = Console()
 
 @main.command("add")
 def add(
@@ -22,5 +26,12 @@ def add(
 def list_beers(style: Optional[str] = None):
     """Lists beers in database"""
     beers = get_beers_from_database()
-    print(beers)
+    table = Table(title="Beerlog")
+    headers = ["id", "name", "style", "rating", "date"]
+    for header in headers:
+        table.add_column(header, style='magenta')
+    for beer in beers:
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
     
