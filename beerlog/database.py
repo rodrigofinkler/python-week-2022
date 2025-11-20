db3c513744f50d5a905a78ce56a94ca553e37b5f
Rodrigo Finkler <finklerrg@gmail.com>
2025-11-20 09:56:32 -0300

    Add snippet to remove sqlalchemy and sqlmodel warnings

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,3 +1,12 @@
+# Remove warnings
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+
+
 from sqlmodel import create_engine, Session
 from beerlog.config import settings
 from beerlog import models
